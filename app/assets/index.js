d864cf76482ef4af4193238c51ce282970bef37a
Joel Beevors <Joel Beevors>
2016-01-08 00:33:19 +0000

    Initial commit, sorted most starting things out

---
+++ app/assets/index.js
@@ -0,0 +1,10 @@
+$(function() {
+
+  $(".animated").each(function(i) {
+    var $item = $(this);
+    setTimeout(function() {
+      $item.addClass("fadeInUp");
+    }, 500 * (i / 2));
+  });
+
+});
---
+++ index.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html><?php
+  include 'php/html_head.php';
+?><body>
+
+<div class="wrapper">
+  <div class="header">
+  </div>
+  <section class="main--section">
+    <nav>
+      <ul class="navigation">
+        <li>
+          <a href="">Home</a>
+        </li><!--
+        --><li>
+          <a href="">About Us</a>
+        </li><!--
+        --><li>
+          <a href="">Events</a>
+        </li><!--
+        --><li>
+          <a href="">Shop</a>
+        </li><!--
+        --><li>
+          <a href="">Contact</a>
+        </li>
+      </ul>
+      <div class="social">
+        <p class="social_tagline">Keep Up-To-Date On Social Media!</p>
+        <ul>
+          <li class="social_button --fb">
+            <a href="">Facebook</a>
+          </li><!--
+          --><li class="social_button --tw">
+            <a href="">Twitter</a>
+          </li><!--
+          --><li class="social_button --ig">
+            <a href="">Instagram</a>
+          </li>
+        </ul>
+      </div>
+    </nav><!--
+    --><div class="content">
+      <section class="animated">
+        <h1>Section Header</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Et, numquam. Deserunt qui, eum facilis iure. Laborum, excepturi quibusdam illo voluptatem corrupti eum ducimus veniam, quis esse provident recusandae iusto possimus!</p>
+        <p>Illum sunt dolor mollitia optio quis alias quas reprehenderit, culpa maxime tenetur, nam! Dolorem minima modi maiores quasi nobis amet nulla eveniet harum beatae, tenetur unde, hic fugit vero et.</p>
+        <p>Officia tempora aperiam quasi enim sequi, repellat sit veritatis, asperiores aliquid maiores minima sunt, laboriosam! Tempore esse doloremque mollitia voluptas, expedita dolores saepe, a nisi voluptatibus ipsam earum error culpa!</p>
+      </section>
+      <section class="animated">
+        <h1>Section Header</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Recusandae ab deleniti odit impedit est maiores natus doloremque aperiam commodi, esse beatae ratione molestias adipisci eos ducimus, quidem sapiente ad at?</p>
+        <p>Totam dolor hic, vero minus numquam. Fuga, placeat? Quisquam culpa numquam repudiandae fuga perferendis, explicabo corporis possimus aliquid eius facere. Dicta quaerat aut doloremque laudantium minus neque quas ipsam, tempore.</p>
+        <img src="http://placehold.it/450x200/A90AFF/FFFFFF" />
+        <p>Qui nisi totam mollitia ea repudiandae minima nam sapiente officiis, impedit dignissimos tempore soluta debitis pariatur ipsa. Eos quos tempora libero placeat excepturi sed ea, voluptatibus aut velit suscipit eligendi.</p>
+      </section>
+      <section class="animated">
+        <h1>Section Header</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias et eum, quo omnis explicabo a eligendi animi dicta ipsum vel, molestias quae beatae laudantium minima, repudiandae quia? Hic, reiciendis, labore?</p>
+        <p>Porro quia dolorem impedit nam, delectus error soluta quae illo sunt, officiis, pariatur quaerat, nihil ad eius consectetur distinctio sed. Placeat quis minus accusamus libero eaque tenetur sapiente a, soluta?</p>
+        <p>Harum quo obcaecati, iure, libero necessitatibus ratione quos doloribus, incidunt quia sint earum magnam recusandae assumenda excepturi placeat voluptates debitis, totam ad eos laudantium modi dolore nesciunt neque. Facilis, ad.</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quo eius ab ea voluptatum eos, nostrum iure illum ex vero. Illum id quod maxime deleniti pariatur.</p>
+        <p>Cupiditate commodi, est culpa facilis dolorum aliquam totam maxime, quae quam perferendis ex. Dolores provident minima modi itaque iure necessitatibus optio illo architecto amet quasi!</p>
+      </section>
+    </div>
+  </section>
+</div>
+    <script src='bower_components/jquery/dist/jquery.min.js'></script>
+    <script src="public/index.js"></script>
+  </body>
+</html>
